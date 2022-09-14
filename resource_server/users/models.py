7794c8f1e4cd8b48f0228e32a74737a41272aaa1
sujain <sujain@roku.com>
2022-09-14 17:18:40 -0500

    initial commit

---
+++ resource_server/users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    class Meta:
+        managed = False
+        db_table = "users_myuser"
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def __str__(self):
+        return self.email
